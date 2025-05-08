dff14110b10e4d690cb3505f1f52c1d50c123a59
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-05-08 16:28:41 -0700

    feature item routes in the item_routes.py

--- web/routes/item_routes.py
+++ web/routes/item_routes.py
@@ -1,60 +1,70 @@
 from flask import jsonify, request
 from web import app, db
 from web.model.Item import Item
+import uuid
 
-@app.route("/api/users", methods=["GET"])
-def allItems():
+@app.route("/api/items", methods=["GET"])
+def getAllItems():
   items = Item.query.all()
+
   if items is None:
-    return jsonify({"error": "Cannot find any items."}), 404
-  return jsonify([{"id": item.id, "name": item.name, "price": item.price} for item in items]), 200
+    return jsonify({"error": "Cannot find any items"}), 400
 
-@app.route("/api/users/add", methods=["POST"])
-def addItem():
-  data = request.json 
-  name = data.get('name')
-  price = data.get('price')
+  return jsonify([{"id": item.id, "name": item.name, "price": item.price, "createdAt": item.createdAt, "updatedAt": item.updatedAt, "owner": item.owner} for item in items]), 200
 
-  if name is None and len(name) < 3:
-    return jsonify({"error": "Please provide a valid name."}), 400
+
+@app.route("/api/item/add/<uuid:UserId>", methods=["POST"])
+def addItem(UserId):
+  data = request.json
+  name = data.get("name")
+  price = data.get("price")
+
+  if len(name) < 3 and name is None:
+    return jsonify({"error": "Invalid Item Name! Please provide a valid name"}), 400
   
-  if price < 0:
-    return jsonify({"error": "Please provide a valid price."}), 400
+  if price < 0 and price is None:
+    return jsonify({"error": "Invalid Price! Please provide a valid price"}), 400
 
-  item = Item(name=name, price=price)
+  item = Item(name=name, price=price, owner=UserId)
+
+  if item is None:
+    return jsonify({"error": "Invalid Item Created!"}), 400
+
   db.session.add(item)
   db.session.commit()
-  return jsonify({"message": "Item is added Successfully", "success": True}), 201
 
-@app.route("/api/users/<int:id>", methods=["GET"])
-def singleItem(id):
-  item = Item.query.get(id)
+  return jsonify({"message": "Item Created Successfully!"}), 201
 
-  if item is None:
-    return jsonify({"error": "Cannot find the item"}), 404
-
-  return jsonify({"id": item.id, "name": item.name, "price": item.price}), 200
-
-@app.route("/api/users/<int:id>", methods=["PUT"])
+@app.route("/api/item/update/<uuid:id>", methods=["PUT"])
 def updateItem(id):
-  data = request.json
   item = Item.query.get(id)
 
   if item is None:
     return jsonify({"error": "Cannot find the item"}), 404
-  
+
+  data = request.json 
   item.name = data.get("name", item.name)
   item.price = data.get("price", item.price)
 
-  db.session.commit()
-  return jsonify({"message": "Item is updated Successfully"}), 200
+  return jsonify({"error": "Item updated Successfully!"}), 200
 
-@app.route("/api/users/<int:id>", methods=["DELETE"])
-def deleteItem(id):
+@app.route("/api/item/<uuid:id>", methods=["GET"])
+def getSingleItem(id):
   item = Item.query.get(id)
+
   if item is None:
     return jsonify({"error": "Cannot find the item"}), 404
   
+  return jsonify({"id": item.id, "name": item.name, "price": item.price, "createdAt": item.createdAt, "updatedAt": item.updatedAt, owner: item.owner}), 200
+
+@app.route("/api/item/delete/<uuid:id>", methods=["DELETE"])
+def removeItem(id):
+  item = Item.query.get(id)
+
+  if item is None:
+    return jsonify({"error": "Cannot find the item"}), 404
+
   db.session.delete(item)
   db.session.commit()
-  return jsonify({"message": "Item is deleted Successfully"}), 200
+
+  return jsonify({"message": "Item has Deleted Successfully!"}), 200
